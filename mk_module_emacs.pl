167b627530f552fdf45efe519f335ba07a1fc477
Jinfengyi <kinhouki@gmail.com>
2016-12-11 21:43:03 +0900

    add intent

--- mk_module_emacs.pl
+++ mk_module_emacs.pl
@@ -19,15 +19,27 @@
 #################################################################
 
 $file = "@ARGV";
-open (IN, $file);
+$intent_num = 3;
+
+####################
+##     INTENT     ##
+####################
+$speace = " ";
+$intent = " ";
+for (my $i = 0; $i < $intent_num; $i++){
+    $intent = $intent.$speace;
+}
+
+
 #####################
 ## make out file   ##
 #####################
+open (IN, $file);
 while (my $line = <IN>){
     my($IO,$outfile,$bit) = split(/,/,$line);
     if($IO eq "filename"){
         $module_file = "$outfile";
-        print "make file : $outfile"
+        print "Make file      : $outfile"
     }
 };
 close(IN);
@@ -36,6 +48,8 @@ close(OUT);
 ###############################
 ##  write  file for emacs   ###
 ###############################
+$Input_num = 0;
+$Output_num = 0;
 open(OUT,">$module_file");
 open (IN, $file);
 while (my $line=<IN>){
@@ -44,11 +58,13 @@ while (my $line=<IN>){
    my($IO,$pin,$bit) = split(/,/,$line);
    $bit = $bit - 1;
    if($IO eq "module"){
-       print"module name: $pin\n";
-       print OUT "module $pin ( /*AUTOARG*/\n    );  \n\n";
+       print"Module name    : $pin\n";
+       print OUT "module $pin ( /*AUTOARG*/\n$intent);  \n\n";
    }
    if($IO eq "input"){
-       print OUT  "   input     ";
+       $Input_num ++;
+       print OUT "$intent";
+       print OUT  "input     ";
        if ($bit eq 0){
            print OUT "           ";
        }
@@ -61,10 +77,12 @@ while (my $line=<IN>){
                print OUT  "[$bit:0]      ";
            }
        }
-       print OUT  "$pin\n";
+       print OUT  "$pin;\n";
    }
    if($IO eq "output"){
-       print OUT  "   output    ";
+       $Output_num ++;
+       print OUT "$intent";
+       print OUT  "output    ";
        if ($bit eq 0){
            print OUT "           ";
        }
@@ -77,10 +95,12 @@ while (my $line=<IN>){
                print OUT  "[$bit:0]      ";
            }
        }
-       print OUT  "$pin\n";
+       print OUT  "$pin;\n";
    }
 };
-print OUT "\n\n endmodule\n";
+print OUT "\nendmodule\n";
+print "Input Port Num : $Input_num\n";
+print "Output Port Num: $Output_num\n";
 close(IN);
 close(OUT);
 
